8b1732091cbbdcdd6e10d0bda2889cb1095b8bc8
LilianDamico <lfons@DESKTOP-552EQIA>
2024-02-02 06:34:10 -0300

    0202

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../src/pages/home/Home';
+import Kanban from './pages/kanban/Kanban';
+import Listas from './pages/listas/Listas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/kanban' element={<Kanban />}/>
+        <Route path='/listas' element={<Listas />} />
+      </Routes>
+    </Router>
   );
 }
 
